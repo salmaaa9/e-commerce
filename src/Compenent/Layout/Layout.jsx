10540441f47b93574f80975d0bb05b4aba7606ed
salmaaa9 <smsmss279201@yahoo.com>
2024-09-11 16:55:22 +0300

    modified layout padding

--- src/Compenent/Layout/Layout.jsx
+++ src/Compenent/Layout/Layout.jsx
@@ -14,7 +14,7 @@ function Layout() {
     <>
      <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow p-20">
+      <div className="flex-grow sm:p-5 md:p-20">
         <Outlet />
       </div>
       <Footer />
